3a7fe4ee53c6780520e0b9d2ed197a95daca7c72
Ivan Isekeev <ivanisekeev0@gmail.com>
2023-12-17 21:30:03 +0600

    Update private_executable_languages.toml, private_init.lua, and run_once_before_install-android-packages.sh.tmpl

--- dot_config/private_nvim/private_init.lua
+++ dot_config/private_nvim/private_init.lua
@@ -12,39 +12,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	{
-		'chipsenkbeil/distant.nvim', 
-		branch = 'v0.3',
-		config = function()
-			require('distant'):setup({    
-				manager = {
-					network = {
-						unix_socket = '/data/data/com.termux/files/run/distant.socket'
-					}
-				},
-				search = {
-					paths = { '/home/frontend/test/gifki-rf' },
-					pagination = 1,
-					limit = 1
-				},
-				servers = {
-					["*"] = {
-						lsp = {
-							['gifs.ru'] = {
-								cmd = { '/usr/bin/vue-language-server' },
-								filetypes = { 'vue' },
-								root_dir = '/home/frontend/test/gifki-rf',
-								typescript = {
-									tsdk = '/usr/lib/node_modules/typescript/lib',
-								}
-							}
-						},
-						cwd = "/home/frontend/test/gifki-rf",
-					}
-				}
-			})
-		end
-	},
+	{ "neovim/nvim-lspconfig" },
 	{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
 	{
 		'nvim-telescope/telescope.nvim', tag = '0.1.4',
@@ -58,19 +26,17 @@ require("lazy").setup({
 						override_file_sorter = true,  
 						case_mode = "smart_case",        
 					},
-					distant = {
-						search = {
-							paths = { '/home/frontend/test/gifki-rf' },
-							pagination = 1,
-							limit = 1
-						}
-					}
 				}
 			})
 			require('telescope').load_extension('fzf')
-			require('telescope').load_extension('distant')
 		end
-	}		
+	},		
+	{ "navarasu/onedark.nvim", 
+		config = function()
+			require('onedark').load() 
+			require('onedark').setup({ style = 'light' }) 
+		end
+	}
 })
 
 vim.g.mapleader = " " 
@@ -84,7 +50,6 @@ vim.bo.tabstop = 3
 
 local builtin = require('telescope.builtin')
 local telescope = require('telescope')
-vim.keymap.set('n', '<leader>f', telescope.extensions.distant.search, {})
 vim.keymap.set('n', '<leader>/', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>b', builtin.buffers, {})
 
@@ -102,4 +67,60 @@ vim.diagnostic.config({
             )
         end,
     },
+})
+
+
+-- Setup language servers.
+local lspconfig = require('lspconfig')
+lspconfig.tsserver.setup {}
+lspconfig.rust_analyzer.setup {
+  -- Server-specific settings. See `:help lspconfig-setup`
+  settings = {
+    ['rust-analyzer'] = {},
+		['vue'] = {
+				filetypes = { 'vue', 'ts', 'js' },
+				typescript = {
+					tsdk = '/usr/lib/node_modules/typescript/lib',
+				}
+		}
+  },
+}
+
+
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
 })
