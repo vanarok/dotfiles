dff5eef126269e190cd8ab3b183fe4de85d0aa6e
Ivan Isekeev <ivanisekeev0@gmail.com>
2023-12-27 17:08:10 +0600

    .

--- dot_config/private_nvim/private_init.lua
+++ dot_config/private_nvim/private_init.lua
@@ -73,17 +73,13 @@ vim.diagnostic.config({
 -- Setup language servers.
 local lspconfig = require('lspconfig')
 lspconfig.tsserver.setup {}
-lspconfig.rust_analyzer.setup {
-  -- Server-specific settings. See `:help lspconfig-setup`
-  settings = {
-    ['rust-analyzer'] = {},
-		['vue'] = {
-				filetypes = { 'vue', 'ts', 'js' },
-				typescript = {
-					tsdk = '/usr/lib/node_modules/typescript/lib',
-				}
+lspconfig.volar.setup {
+	filetypes = { 'typescript', 'javascript', 'vue' },
+  init_options = {
+		typescript = {
+			tsdk = '/data/data/com.termux/files/home/.local/share/pnpm/global/5/node_modules/typescript/lib',
 		}
-  },
+	}
 }
 
 
@@ -117,7 +113,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end, opts)
     vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>a', vim.lsp.buf.code_action, opts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
     vim.keymap.set('n', '<space>f', function()
       vim.lsp.buf.format { async = true }
