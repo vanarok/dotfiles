124c9b98bd353ac804fd768edf1937a7792c750c
Ivan Isekeev <hi@ivanisekeev.com>
2025-01-04 13:13:09 +0600

    Update

---
+++ dot_config/autostart/executable_rclone-google.sh
@@ -0,0 +1,1 @@
+rclone mount google:/Files ~/Cloud --vfs-cache-max-size=150G --vfs-cache-mode full --vfs-cache-max-age 72h  --vfs-cache-poll-interval 1m --vfs-write-back 20s --drive-acknowledge-abuse
